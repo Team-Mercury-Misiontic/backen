5ea9fba9ff94140893d76366791e1748ea9c2552
Daniel Melo <danielmelo276@gmail.com>
2021-11-16 18:32:07 -0500

    adding new dependencies

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,15 +1,13 @@
 import { connect } from 'mongoose';
 
 const connectDB = async () => {
-	return await connect(
-		'UrlLink'
-	)
-    .then(() => {
-        console.log('Connection succesfully');
-    })
-    .catch((err) => {
-        console.error(`Something went wrong ${err}`)
-    })
+	return await connect(process.env.DATABASE_URL)
+		.then(() => {
+			console.log('Connection succesfully');
+		})
+		.catch((err) => {
+			console.error(`Something went wrong ${err}`);
+		});
 };
 
 export default connectDB;
