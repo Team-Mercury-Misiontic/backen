e5715661b0ce775a2ade1b9c96f49a91c5778ab0
Daniel Melo <danielmelo276@gmail.com>
2021-12-08 20:36:33 -0500

    utils import

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { resolvers } from './graphql/resolvers.js';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { validateToken } from './utils/tokenUtils'
 
 dotenv.config();
 
