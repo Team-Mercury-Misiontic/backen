4491cf9e70c55ed6418c85a3ae83323f59bb7b83
CaroKan <quindioseguro@gmail.com>
2021-12-10 17:58:21 -0500

    Se actualiza el contexto para verificar el token enviado por el front

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -2,25 +2,26 @@ import jwt from 'jsonwebtoken';
 
 const validateToken = (token) => {
   if (token) {
-    const verification = jwt.verify(token, 'secret', (err, data) => {
+    const verification = jwt.verify(token, 'mercury', (err, data) => {
       if (data) {
         return {
           data: data,
         };
       }
+      
       if (err) {
         return {
           error: err,
         };
       }
     });
-    console.log(verification, token);
+    console.log("info del token",verification);
     return verification;
   }
 };
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, 'palabraSecreta', {
+  return jwt.sign(payload, 'mercury', {
     expiresIn: '24h',
   });
 };
